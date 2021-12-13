c7a808875e929d343cf6866f95795b2a0eb98efc
GGunter * <ggunter.game@gmail.com>
2021-12-13 19:27:50 -0300

    Introdução a variáveis

---
+++ curso-java-basico-eclipse/src/com/ggunter/cursojava/aula10/Variaveis.java
@@ -0,0 +1,18 @@
+package com.ggunter.cursojava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		int idade = 20;
+		String nome = "Adriano";
+		String nomeDoChorro = "Pit";
+		
+		idade = 25;
+		
+		System.out.println("Idade = " + idade);
+		System.out.println("Nome = " + nome);
+
+	}
+
+}
